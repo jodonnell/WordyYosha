2e9507818777d795e2f9284c7282d748f15cf877
Jacob O'Donnell <jacobodonnell@gmail.com>
2014-06-12 08:39:10 -0400

    added letter creator

--- WordyYosha/GameScene.swift
+++ WordyYosha/GameScene.swift
@@ -11,31 +11,31 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+        for x in (0..8) {
+            for y in (0..8) {
+                self.addChild(LetterCreator.create(x, y: y))
+            }
+        }
+
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
+            // let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            // let sprite = SKSpriteNode(imageNamed:"Spaceship")
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            // sprite.xScale = 0.5
+            // sprite.yScale = 0.5
+            // sprite.position = location
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            // let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
             
-            sprite.runAction(SKAction.repeatActionForever(action))
+            // sprite.runAction(SKAction.repeatActionForever(action))
             
-            self.addChild(sprite)
+            // self.addChild(sprite)
         }
     }
    
---
+++ WordyYosha/LetterCreator.swift
@@ -0,0 +1,53 @@
+//
+//  LetterCreator.swift
+//  WordyYosha
+//
+//  Created by Jacob O'Donnell on 6/12/14.
+//  Copyright (c) 2014 Jacob O'Donnell. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class LetterCreator {
+    class func create(x: Int, y: Int) -> SKLabelNode {
+        let myLabel = SKLabelNode(fontNamed:"Al Nile")
+        myLabel.text = self.randomLetter()
+        let fontSize = 37
+        let padding = 30
+        myLabel.fontSize = CGFloat(fontSize)
+        myLabel.position = CGPoint(x: x * fontSize + padding, y: y * (fontSize + 3) + padding)
+        
+        return myLabel
+    }
+
+    class func randomLetter() -> String {
+        let cower = arc4random() % 100000
+        if cower < 8167 { return "A" }
+        if cower < 9659 { return "B" }
+        if cower < 12441 { return "C" }
+        if cower < 16694 { return "D" }
+        if cower < 29694 { return "E" }
+        if cower < 31922 { return "F" }
+        if cower < 33937 { return "G" }
+        if cower < 40031 { return "H" }
+        if cower < 46997 { return "I" }
+        if cower < 47150 { return "J" }
+        if cower < 47922 { return "K" }
+        if cower < 51947 { return "L" }
+        if cower < 54353 { return "M" }
+        if cower < 61102 { return "N" }
+        if cower < 68609 { return "O" }
+        if cower < 70538 { return "P" }
+        if cower < 70633 { return "Q" }
+        if cower < 76620 { return "R" }
+        if cower < 82947 { return "S" }
+        if cower < 92003 { return "T" }
+        if cower < 94761 { return "U" }
+        if cower < 95739 { return "V" }
+        if cower < 98099 { return "W" }
+        if cower < 98249 { return "X" }
+        if cower < 99930 { return "Y" }
+        return "Z"
+    }
+}
