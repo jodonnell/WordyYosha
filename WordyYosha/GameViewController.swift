0b53d614c1d9c32e4f05b83120d1744693b05aee
Jacob O'Donnell <jacobodonnell@gmail.com>
2014-06-13 12:16:30 -0400

    should not autorotate

--- WordyYosha/GameViewController.swift
+++ WordyYosha/GameViewController.swift
@@ -33,7 +33,7 @@ class GameViewController: UIViewController {
     }
 
     override func shouldAutorotate() -> Bool {
-        return true
+        return false
     }
 
     override func supportedInterfaceOrientations() -> Int {
